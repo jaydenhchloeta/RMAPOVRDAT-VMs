c8925aaea3c31d79da7f532aed839f87e183cd8c
Jayden Hill <jayden.hill@chloeta.com>
2026-06-10 16:33:26 -0500

    updated rdatastore

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "azurerm_windows_virtual_machine" "RDatastore01" {
   os_disk {
     caching = "ReadWrite"
     storage_account_type = "Premium_LRS"
-    disk_size_gb = 1024
+    disk_size_gb = 2048
    }
 
    source_image_reference {
